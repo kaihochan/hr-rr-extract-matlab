376675bf8f56fbd036b214096f7e28711bea7775
Kai Ho CHAN <kaihojohnchan@gmail.com>
2023-03-01 22:38:11 +0800

    MZI full code uploaded, MZI related data added
    Need to fine tune the filter value of MZI extraction function, the result is weird

--- MZI_Extraction.m
+++ MZI_Extraction.m
@@ -1,24 +1,45 @@
-function [HR,RR] = MZI_Extraction(dataArray,showFigure)
-%SAGNAC Extract HR and RR from Mach–Zehnder interferometer
-
-    % add demod here!
+function [HR,RR,refHR] = MZI_Extraction(PVDFdata,showFigure)
+%MZI Extract HR and RR from Mach–Zehnder interferometer
+    % load data from excel file
+    % col 1 is reference signal
+    % col 2-4 are phased sensor signal
+    refRaw = PVDFdata(1:end,1);
+    sen1 = PVDFdata(1:end,2);
+    sen2 = PVDFdata(1:end,3);
+    sen3 = PVDFdata(1:end,4);
 
     % length of data array, 30 sec
-    L = length(dataArray);
+    L = length(sen1);
     % sample frequency, either 5kHz or 1kHz
     Fs = L/30;
     % sample period
     T = 1/Fs;
     
+    % demoduation on phased signal
+    sen = (sen1+sen2+sen3)/3;
+    a = sen1-sen;
+    b = sen2-sen;
+    c = sen3-sen;
+    d = diff(a)*Fs;
+    e = diff(b)*Fs;
+    f = diff(c)*Fs;
+    g = a(1:end-1).*(e-f);
+    h = b(1:end-1).*(f-d);
+    i = c(1:end-1).*(d-e);
+    z = g+h+i;
+    m = a.^2+b.^2+c.^2;
+    u = z./(m(1:end-1));
+    demodArray = -cumtrapz(u);
+    
     % bandpass filter for HR
     % acceptance range 0.83Hz to 2.5Hz
     % sampling range 5kHz or 1kHz, depends on data array
-    hrFlt = bandpass(dataArray,[0.83 2.5],Fs);
+    hrFlt = bandpass(demodArray,[0.83 2.5],Fs);
     
     % bandpass filter for RR
     % acceptance range 0.14Hz to 0.58Hz
     % sampling range 2.5kHz or 0.5kHz, depends on data array
-    rrFlt = bandpass(dataArray,[0.14 0.58],Fs/2);
+    rrFlt = bandpass(demodArray,[0.14 0.42],Fs/2);
     
     % FFT on HR filtered sensor signal
     hrY = fft(hrFlt);
@@ -33,6 +54,13 @@ function [HR,RR] = MZI_Extraction(dataArray,showFigure)
     rrP1 = rrP2(1:L/2+1);
     rrP1(2:end-1) = 2*rrP1(2:end-1);
     rrf = Fs*(0:(L/2))/L;
+
+    % FFT on reference sensor signal
+    refhrY = fft(refRaw);
+    refhrP2 = abs(refhrY/L);
+    refhrP1 = refhrP2(1:L/2+1);
+    refhrP1(2:end-1) = 2*refhrP1(2:end-1);
+    refhrf = Fs*(0:(L/2))/L;
     
     % calculate HR based on filtered sensor signal
     [~, hrfL] = max(hrP1(2:200));
@@ -44,21 +72,26 @@ function [HR,RR] = MZI_Extraction(dataArray,showFigure)
     fRR = rrfL*Fs/L;
     RR = 60/(1/(fRR));
 
+    % calculate HR based on reference sensor signal
+    [~, refhrfL] = max(refhrP1(2:200));
+    reffHR = refhrfL*Fs/L;
+    refHR = 60/(1/(reffHR));
+
     % show raw signal, filtered signal and filtered amplitude spectrum
     if showFigure
         figure; tiledlayout(5,1,'TileSpacing','tight','Padding','tight'); 
 
-        nexttile; plot(0:T:30-T,dataArray); 
+        nexttile; plot(0:T:30-2*T,demodArray); 
         title('Sensor Raw Signal');
         xlabel('s')
         ylabel('V')
 
-        nexttile; plot(0:T:30-T,hrFlt); 
+        nexttile; plot(0:T:30-2*T,hrFlt); 
         title('HR Filtered Sensor Signal');
         xlabel('s')
         ylabel('V')
 
-        nexttile; plot(0:T:30-T,rrFlt); 
+        nexttile; plot(0:T:30-2*T,rrFlt); 
         title('RR Filtered Sensor Signal');
         xlabel('s')
         ylabel('V')
